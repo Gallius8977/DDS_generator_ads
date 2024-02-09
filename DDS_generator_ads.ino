96a4bd12d72b3214a6044639f257389d12ce4134
N/tro <119893995+Gallius8977@users.noreply.github.com>
2024-02-09 15:10:52 +0100

    Update DDS_generator_ads.ino
    voltmeres, valami rossz a hrdveren. ha az a1 diodat(potmeter) hozzaerintem a0 bemenethez, a0-an latszik a potmeter

--- DDS_generator_ads.ino
+++ DDS_generator_ads.ino
@@ -12,53 +12,61 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-// Pins for SPI comm with the AD9833 IC
 BluetoothSerial SerialBT;
-int PIN_DATA = 18;	///< SPI Data pin number
-int PIN_CLK = 17;		///< SPI Clock pin number
-int PIN_FSYNC = 5;	///< SPI Load pin number
+int PIN_DATA = 18;  ///< SPI Data pin number
+int PIN_CLK = 17;   ///< SPI Clock pin number
+int PIN_FSYNC = 5;  ///< SPI Load pin number
 String S = "";
-MD_AD9833	AD(PIN_DATA, PIN_CLK, PIN_FSYNC); // Arbitrary SPI pins
+int16_t adc0, adc1;
+MD_AD9833 AD(PIN_DATA, PIN_CLK, PIN_FSYNC); // Arbitrary SPI pins
 
 Adafruit_ADS1115 ads;
+
+const float VREF = 4.096;  // Voltage reference
+const float GAIN = 1.0;    // ADC gain
 
 void setup(void)
 {
   Wire.begin(SDA_PIN, SCL_PIN);
-  Serial.begin(9600);
+  Serial.begin(115200);  // Increase baud rate for faster communication
   SerialBT.begin("EDDY_C");
   ads.begin();
-  ads.setGain(GAIN_TWO);
-	AD.begin();
+  ads.setGain(GAIN_ONE);  // Set the gain (1x) for the ADS1115
+  AD.begin();
   AD.setMode(MD_AD9833::MODE_OFF);
 }
 
 void loop(void)
 {
-  if  (Serial.available() > 0) {
-      S = Serial.readString() ;
-      S.trim();
-      Serial.println(S);
-  	  int v =S.toInt();
-      AD.setMode(MD_AD9833::MODE_SINE);
-      AD.setFrequency(MD_AD9833::CHAN_0, v);
+  if (Serial.available() > 0) {
+    S = Serial.readString();
+    S.trim();
+    Serial.println(S);
+    int v = S.toInt();
+    AD.setMode(MD_AD9833::MODE_SINE);
+    AD.setFrequency(MD_AD9833::CHAN_0, v);
   }
-  
-  if  (SerialBT.available() > 0) {
-      S = SerialBT.readString() ;
-      S.trim();
-      SerialBT.println(S);
-      Serial.println(S);
-  	  int v =S.toInt();
-      AD.setMode(MD_AD9833::MODE_SINE);
-      AD.setFrequency(MD_AD9833::CHAN_0, v);
+
+  if (SerialBT.available() > 0) {
+    S = SerialBT.readString();
+    S.trim();
+    SerialBT.println(S);
+    Serial.println(S);
+    int v = S.toInt();
+    AD.setMode(MD_AD9833::MODE_SINE);
+    AD.setFrequency(MD_AD9833::CHAN_0, v);
   }
-  int16_t adc0, adc1;
+
   adc0 = ads.readADC_SingleEnded(0);
   adc1 = ads.readADC_SingleEnded(1);
+
+  float millivolts_adc0 = adc0 * (VREF / 32767) * GAIN * 1000;
+  float millivolts_adc1 = adc1 * (VREF / 32767) * GAIN *1000;
+
   Serial.print("ADC 0 : ");
-  Serial.println(adc0);
+  Serial.println(millivolts_adc0);
   Serial.print("ADC 1 : ");
-  Serial.println(adc1);
-  delay(1000);
+  Serial.println(millivolts_adc1);
+
+  delay(10);  // Adjust delay for the desired sample rate
 }
